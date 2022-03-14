8c5069fa868c1d4749a86631553d1f6b84059ab9
Thanh Tungg <trantungg910@gmail.com>
2022-03-14 14:46:00 +0700

    Feat: models products

---
+++ backend/src/models/Product.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			minlength: 5,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+			minlength: 10,
+		},
+		saleoff: {
+			type: Number,
+			default: 0,
+		},
+		quantity: {
+			type: Number,
+			default: 1,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+export default mongoose.model("Products", productSchema);
