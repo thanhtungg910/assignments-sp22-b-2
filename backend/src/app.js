357a9f9dc743cdf70eb0a773a5e26f949db51f0d
Thanh Tungg <trantungg910@gmail.com>
2022-03-14 14:38:27 +0700

    Feat: connect to db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import db from "./config/connect";
 dotenv.config();
 const app = express();
-
 app.use(morgan());
-app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("<h1>HOME PAGE</h1>");
+});
+
+//CONNECT DB
+db.connect();
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
---
+++ backend/src/config/connect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const db = {
+	async connect() {
+		try {
+			await mongoose.connect(process.env.MONGO_URL, () => {
+				console.log("-----> Connected to db <-----");
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	},
+};
+export default db;
