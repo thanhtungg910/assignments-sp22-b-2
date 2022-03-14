9da09f3c95ad2c52c10c5a0f84ccc4624de9d7d9
Thanh Tungg <trantungg910@gmail.com>
2022-03-14 18:08:30 +0700

    Feat: categories controllers

--- backend/src/controllers/categories.js
+++ backend/src/controllers/categories.js
@@ -1,10 +1,28 @@
-import categoriModel from "../models/Category";
+import categoryModel from "../models/Category";
 
 const categoryControll = {
+	// CATEGORY
+	async getAll(req, res) {
+		try {
+			const categori = await categoryModel.find().exec();
+			res.status(200).json(categori);
+		} catch (error) {
+			res.status(400).json({ message: error });
+		}
+	},
 	// CREATE CATEGORY
 	create(req, res) {
-		const categori = new categoriModel(req.body).save();
+		const categori = new categoryModel(req.body).save();
 		res.status(200).json(categori);
+	},
+	// GET CATEGORY
+	async getCategory(req, res) {
+		try {
+			const category = await categoryModel.find({ _id: req.params.id }).exec();
+			res.status(200).json(category);
+		} catch (error) {
+			res.status(400).json({ message: error });
+		}
 	},
 };
 export default categoryControll;
--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -4,8 +4,9 @@ import check from "../middleware/check";
 const router = Router();
 
 // GET ALL
-router.get("/", check);
+router.get("/", check, categoryControll.getAll);
 // DETAILS
+router.get("/:id", check, categoryControll.getCategory);
 // CREATE
 router.post("/", check, categoryControll.create);
 export default router;
