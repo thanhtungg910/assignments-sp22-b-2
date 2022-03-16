36e125e7f072ba3f0d84d8cb749aad20bd6fc408
Thanh Tungg <trantungg910@gmail.com>
2022-03-16 21:07:16 +0700

    Fix: models

--- backend/src/models/Category.js
+++ backend/src/models/Category.js
@@ -4,11 +4,9 @@ const CategorySchema = mongoose.Schema({
 	title: {
 		type: String,
 		required: true,
-		minlength: 5,
 	},
 	slug: {
 		type: String,
-		minlength: 5,
 		lowercase: true,
 		index: true,
 		unique: true,
--- frontend/src/pages/products/ProductsPage.tsx
+++ frontend/src/pages/products/ProductsPage.tsx
@@ -5,15 +5,13 @@ import Product from "../../components/product/Product";
 import AccordionProduct from "../../components/product/Accordion ";
 import Flexs from "../../components/layouts/Flexs";
 import Grids from "../../components/layouts/Grids";
-type Props = {};
-const ProductsPage: React.FC = (props: Props) => {
+
+const ProductsPage: React.FC = () => {
 	const [toggle, setToggle] = useState<boolean>(false);
 	return (
 		<div>
 			<BasicBreadcrumbs />
-
 			<Filters toggle={toggle} onClick={setToggle} />
-
 			<Flexs className="px-10 my-10">
 				{!toggle && (
 					<div className="w-[30%] transition-all">
@@ -29,19 +27,3 @@ const ProductsPage: React.FC = (props: Props) => {
 };
 
 export default ProductsPage;
-/* 
-<div className="flex gap-4 px-10 my-10">
-				{!toggle && (
-					<div className="w-[30%] transition-all">
-						<AccordionProduct />
-					</div>
-				)}
-				<div className="col-span-2 grid grid-cols-4 gap-4 w-full">
-					<Product />
-					<Product />
-					<Product />
-					<Product />
-					<Product />
-					<Product />
-				</div>
-			</div> */
