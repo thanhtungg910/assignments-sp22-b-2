4c2ce4f22c7bf5777b7fb18cce2c29f17f3661b5
Thanh Tungg <trantungg910@gmail.com>
2022-03-18 14:21:42 +0700

    Feat: login form

--- frontend/src/features/SignUp.tsx
+++ frontend/src/features/SignUp.tsx
@@ -1,5 +1,6 @@
-import { Button, FormGroup, TextField } from "@mui/material";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { Button, FormControl, TextField } from "@mui/material";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 interface IFormInput {
 	email: String;
@@ -7,39 +8,51 @@ interface IFormInput {
 }
 
 export default function SignUp() {
-	const { register, handleSubmit } = useForm<IFormInput>();
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<IFormInput>();
 	const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
 	return (
-		<FormGroup
+		<FormControl
 			sx={{
 				width: 350,
 				maxWidth: "100%",
 				paddingLeft: 3,
 				paddingRight: 3,
+				color: "black",
 			}}
-			onSubmit={handleSubmit(onSubmit)}
 		>
-			<TextField
-				sx={{
-					marginBottom: 5,
-				}}
-				fullWidth
-				label="Email"
-				variant="standard"
-				{...register("email")}
-			/>
+			<div className="relative z-0 mb-6 w-full group ">
+				<input
+					type="email"
+					className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+					placeholder=" "
+					{...register("email", { required: true, minLength: 5 })}
+				/>
+				<label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+					Email address
+				</label>
+				{errors.email && <span className="text-red-600">This field is required email</span>}
+			</div>
+			<div className="relative z-0 mb-6 w-full group">
+				<input
+					type="password"
+					className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+					placeholder=" "
+					{...register("password", { required: true, minLength: 8 })}
+				/>
+				<label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+					Password
+				</label>
+				{errors.password && <span className="text-red-600">This field is required password</span>}
+			</div>
 
-			<TextField
-				sx={{
-					marginBottom: 5,
-				}}
-				fullWidth
-				label="Password"
-				variant="standard"
-				{...register("password")}
-			/>
-			<Button variant="contained">Login</Button>
-		</FormGroup>
+			<Button variant="contained" onClick={handleSubmit(onSubmit)}>
+				login
+			</Button>
+		</FormControl>
 	);
 }
