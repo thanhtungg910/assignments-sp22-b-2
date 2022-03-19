3e611ada1c98cb97df467f53847a9440a2d167b7
Thanh Tungg <trantungg910@gmail.com>
2022-03-19 12:51:15 +0700

    Feat: dashboard layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/admin/Dashboard";
 import HomePage from "./pages/home/HomePage";
-import Dashboard from "./pages/layouts/Dashboard";
+import DashboardLayout from "./pages/layouts/DashboardLayout";
 import WebsiteLayout from "./pages/layouts/WebsiteLayout";
 import DetailPage from "./pages/products/DetailPage";
 import ProductsPage from "./pages/products/ProductsPage";
@@ -22,9 +23,9 @@ const App: React.FC = () => {
 					<Route path="intro" element={<h1>intro</h1>}></Route>
 					<Route path="contact" element={<h1>contact</h1>}></Route>
 				</Route>
-				<Route path="/admin" element={<Dashboard />}>
-					<Route index element={<h1>admin</h1>}></Route>
-					<Route path="products" element={<h1>contact</h1>}></Route>
+				<Route path="/admin" element={<DashboardLayout />}>
+					<Route index element={<Dashboard />}></Route>
+					<Route path="products" element={<h1>products</h1>}></Route>
 					<Route path="contact" element={<h1>contact</h1>}></Route>
 				</Route>
 			</Routes>
