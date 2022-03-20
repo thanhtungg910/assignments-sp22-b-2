760f59cc822323bb2f2a517d00c64b6f35d7e1d0
Thanh Tungg <trantungg910@gmail.com>
2022-03-20 11:19:14 +0700

    Feat: react hook form

--- frontend/src/components/admin/product/AddProduct.tsx
+++ frontend/src/components/admin/product/AddProduct.tsx
@@ -21,6 +21,8 @@ import { useForm, Controller } from "react-hook-form";
 import { Theme, useTheme } from "@mui/material/styles";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Chip from "@mui/material/Chip";
+import UploadImages from "./UploadImages";
+import ReactHookFormSelect from "./ReactHookFormSelect";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -58,7 +60,11 @@ function getStyles(name: string, personName: readonly string[], theme: Theme) {
 type Props = {};
 
 const AddProduct = (props: Props) => {
+	const theme = useTheme();
 	const [category, setCategory] = React.useState("");
+	const [sale, setSale] = React.useState("");
+	const [color, setColor] = React.useState<string[]>([]);
+	const [size, setSize] = React.useState<string[]>([]);
 
 	const {
 		control,
@@ -67,17 +73,28 @@ const AddProduct = (props: Props) => {
 		formState: { errors },
 	} = useForm({
 		defaultValues: {
-			firstName: "",
+			title: "",
+			price: "",
+			category: "",
+			images: "",
+			sale: "",
+			amount: "",
+			color: [],
+			size: [],
+			description: "",
 		},
 	});
-	const theme = useTheme();
-	const [personName, setPersonName] = React.useState<string[]>([]);
-
-	const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+	const handleChangeColor = (event: SelectChangeEvent<typeof color>) => {
 		const {
 			target: { value },
 		} = event;
-		setPersonName(
+		setColor(typeof value === "string" ? value.split(",") : value);
+	};
+	const handleChangeSize = (event: SelectChangeEvent<typeof size>) => {
+		const {
+			target: { value },
+		} = event;
+		setSize(
 			// On autofill we get a stringified value.
 			typeof value === "string" ? value.split(",") : value
 		);
@@ -86,229 +103,231 @@ const AddProduct = (props: Props) => {
 	const handleChangeCategory = (event: any) => {
 		setCategory(event.target.value);
 	};
-	const onSubmit: object = (data: object) => {
+	const handleChangeSale = (event: any) => {
+		setSale(event.target.value);
+	};
+	const onSubmit: any = (data: any) => {
 		console.log(data);
-		console.log(`data`);
 	};
 	return (
-		<Grid container spacing={3}>
-			<Grid item xs={12} md={8} lg={9}>
-				<Paper
-					sx={{
-						p: 2,
-						minHeight: 240,
-					}}
-				>
-					<Typography variant="h5">Add products</Typography>
-					<Grid
-						xs={12}
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Grid container spacing={3}>
+				<Grid item xs={12} md={8} lg={9}>
+					<Paper
 						sx={{
 							p: 2,
 							minHeight: 240,
 						}}
 					>
-						<Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-							<Grid item xs={6}>
-								<TextField fullWidth label="Title" variant="standard" />
-							</Grid>
-							<Grid item xs={6}>
-								<TextField fullWidth label="Price" type="number" variant="standard" />
-							</Grid>
-							<Grid item xs={6}>
-								<FormControl variant="standard" fullWidth>
-									<InputLabel id="demo-simple-select-standard-label">Categories</InputLabel>
-									<Select
-										labelId="demo-simple-select-standard-label"
-										id="demo-simple-select-standard"
-										value={category}
-										onChange={handleChangeCategory}
-										label="category"
-										fullWidth
-									>
-										<MenuItem value="">
-											<em>None</em>
-										</MenuItem>
-										<MenuItem value={10}>Ten</MenuItem>
-										<MenuItem value={20}>Twenty</MenuItem>
-										<MenuItem value={30}>Thirty</MenuItem>
-									</Select>
-								</FormControl>
-							</Grid>
-							<Grid item xs={6}>
-								<FormControl variant="standard" fullWidth>
-									<InputLabel id="demo-simple-select-standard-label">Sale off</InputLabel>
-									<Select
-										labelId="demo-simple-select-standard-label"
-										id="demo-simple-select-standard"
-										value={category}
-										onChange={handleChangeCategory}
-										label="category"
-										fullWidth
-									>
-										<MenuItem value="">
-											<em>None</em>
-										</MenuItem>
-										<MenuItem value={10}>Ten</MenuItem>
-										<MenuItem value={20}>Twenty</MenuItem>
-										<MenuItem value={30}>Thirty</MenuItem>
-									</Select>
-								</FormControl>
-							</Grid>
-							<Grid item xs={6}>
-								<TextField fullWidth label="Amount" variant="standard" />
-							</Grid>
-							<Grid item xs={6}>
-								{/* <TextField fullWidth label="Amount" type="file" variant="standard" /> */}
+						<Typography variant="h5">Add products</Typography>
+						<Grid
+							sx={{
+								p: 2,
+								minHeight: 240,
+							}}
+						>
+							<Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+								<Grid item xs={6}>
+									<Controller
+										name="title"
+										rules={{ required: true }}
+										control={control}
+										render={({ field }) => (
+											<TextField fullWidth label="Title" variant="standard" {...field} />
+										)}
+									/>
+									{errors?.title && <Typography color="error">This is required</Typography>}
+								</Grid>
+								<Grid item xs={6}>
+									<Controller
+										name="price"
+										rules={{ required: true }}
+										control={control}
+										render={({ field }) => (
+											<TextField fullWidth label="Price" variant="standard" {...field} />
+										)}
+									/>
+									{errors?.price && <Typography color="error">This is required</Typography>}
+								</Grid>
 
-								<label className="block text-sm font-medium text-gray-700">Cover photo</label>
-								<div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-									<div className="space-y-1 text-center">
-										<img
-											src="https://2.bp.blogspot.com/-muVbmju-gkA/Vir94NirTeI/AAAAAAAAT9c/VoHzHZzQmR4/s1600/placeholder-image.jpg"
-											className="mx-auto h-12 w-12 text-gray-400"
+								<Grid item xs={6}>
+									<Controller
+										name="category"
+										rules={{ required: true }}
+										control={control}
+										render={({ field }) => (
+											<FormControl variant="standard" fullWidth>
+												<InputLabel>Category</InputLabel>
+												<Select
+													value={category}
+													onChange={handleChangeCategory}
+													fullWidth
+													label="category"
+													variant="standard"
+													{...field}
+												>
+													<MenuItem value="">
+														<em>None</em>
+													</MenuItem>
+													<MenuItem value={"Ten"}>Ten</MenuItem>
+													<MenuItem value={"Twenty"}>Twenty</MenuItem>
+													<MenuItem value={"Thirty"}>Thirty</MenuItem>
+												</Select>
+											</FormControl>
+										)}
+									/>
+									{errors?.category && <Typography color="error">This is required</Typography>}
+								</Grid>
+
+								<Grid item xs={6}>
+									<FormControl variant="standard" fullWidth>
+										<InputLabel>Sale off</InputLabel>
+										<Controller
+											name="sale"
+											rules={{ required: true }}
+											control={control}
+											render={({ formState }) => (
+												<Select
+													value={sale}
+													onChange={handleChangeSale}
+													label="Sale"
+													fullWidth
+													{...formState}
+												>
+													<MenuItem value="">
+														<em>None</em>
+													</MenuItem>
+													<MenuItem value={10}>Ten</MenuItem>
+													<MenuItem value={20}>Twenty</MenuItem>
+													<MenuItem value={30}>Thirty</MenuItem>
+												</Select>
+											)}
 										/>
-										<div className="flex text-sm text-gray-600">
-											<label
-												htmlFor="file-upload"
-												className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-											>
-												<span>Upload a file</span>
-												<input
-													id="file-upload"
-													name="file-upload"
-													type="file"
-													className="sr-only"
-												/>
-											</label>
-											<p className="pl-1">or drag and drop</p>
-										</div>
-										<p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-									</div>
-								</div>
+										{errors?.sale && <Typography color="error">This is required</Typography>}
+									</FormControl>
+								</Grid>
+								<Grid item xs={6}>
+									<Controller
+										name="amount"
+										// rules={{ required: true }}
+										control={control}
+										render={({ field }) => (
+											<TextField fullWidth label="Amount" variant="standard" {...field} />
+										)}
+									/>
+									{errors?.amount && <Typography color="error">This is required</Typography>}
+								</Grid>
+								<Grid item xs={6}>
+									<Controller
+										name="images"
+										control={control}
+										rules={{ required: true }}
+										render={({ field }) => <UploadImages field={{ ...field }} />}
+									/>
+									{errors?.images && <Typography color="error">This is required</Typography>}
+								</Grid>
 							</Grid>
 						</Grid>
-					</Grid>
-				</Paper>
-			</Grid>
-			{/**
-			 * Options start
-			 */}
-			<Grid item xs={12} md={4} lg={3}>
-				<Paper
-					sx={{
-						p: 2,
-						display: "flex",
-						flexDirection: "column",
-						rowGap: 2,
-						minHeight: 240,
-					}}
-				>
-					{/* color start */}
-					<FormControl fullWidth>
-						<InputLabel id="demo-multiple-chip-label">Color</InputLabel>
-						<Select
-							labelId="demo-multiple-chip-label"
-							id="demo-multiple-chip"
-							multiple
-							value={personName}
-							onChange={handleChange}
-							input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-							renderValue={(selected) => (
-								<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-									{selected.map((value) => (
-										<Chip key={value} label={value} />
-									))}
-								</Box>
+					</Paper>
+				</Grid>
+				{/**
+				 * Options start
+				 */}
+				<Grid item xs={12} md={4} lg={3}>
+					<Paper
+						sx={{
+							p: 2,
+							display: "flex",
+							flexDirection: "column",
+							rowGap: 2,
+							minHeight: 240,
+						}}
+					>
+						{/* color start */}
+						<Controller
+							name="color"
+							rules={{ required: true }}
+							control={control}
+							render={({ field }) => (
+								<FormControl fullWidth>
+									<InputLabel id="demo-multiple-chip-label">Color</InputLabel>
+									<Select
+										{...field}
+										multiple
+										value={color}
+										onChange={handleChangeColor}
+										input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+										renderValue={(selected) => (
+											<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+												{selected.map((value) => (
+													<Chip key={value} label={value} />
+												))}
+											</Box>
+										)}
+										MenuProps={MenuProps}
+									>
+										{names.map((name) => (
+											<MenuItem key={name} value={name} style={getStyles(name, color, theme)}>
+												{name}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
 							)}
-							MenuProps={MenuProps}
-						>
-							{names.map((name) => (
-								<MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
-									{name}
-								</MenuItem>
-							))}
-						</Select>
-					</FormControl>
-					{/* color end */}
-					{/* Size start */}
-					<FormControl fullWidth>
-						<InputLabel id="demo-multiple-chip-label">Size</InputLabel>
-						<Select
-							labelId="demo-multiple-chip-label"
-							id="demo-multiple-chip"
-							multiple
-							value={personName}
-							onChange={handleChange}
-							input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-							renderValue={(selected) => (
-								<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-									{selected.map((value) => (
-										<Chip key={value} label={value} />
-									))}
-								</Box>
-							)}
-							MenuProps={MenuProps}
-						>
-							{names.map((name) => (
-								<MenuItem key={name} value={name} style={getStyles(name, personName, theme)}>
-									{name}
-								</MenuItem>
-							))}
-						</Select>
-					</FormControl>
-					{/* Size end */}
-				</Paper>
-			</Grid>
+						/>
+						{errors?.color && <Typography color="error">This is required</Typography>}
 
-			{/* Recent Orders */}
-			<Grid item xs={12}>
-				<Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-					<TextField multiline fullWidth rows={4}></TextField>
-				</Paper>
+						{/* color end */}
+						{/* Size start */}
+						<Controller
+							name="size"
+							rules={{ required: true }}
+							control={control}
+							render={({ formState }) => (
+								<FormControl fullWidth>
+									<InputLabel id="demo-multiple-chip-label">Size</InputLabel>
+									<Select
+										labelId="demo-multiple-chip-label"
+										id="demo-multiple-chip"
+										multiple
+										value={size}
+										onChange={handleChangeSize}
+										input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+										renderValue={(selected) => (
+											<Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+												{selected.map((value) => (
+													<Chip key={value} label={value} />
+												))}
+											</Box>
+										)}
+										MenuProps={MenuProps}
+									>
+										{names.map((name) => (
+											<MenuItem key={name} value={name} style={getStyles(name, size, theme)}>
+												{name}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
+							)}
+						/>
+						{errors?.size && <Typography color="error">This is required</Typography>}
+
+						{/* Size end */}
+						<Button color="primary" variant="contained" type="submit">
+							Add
+						</Button>
+					</Paper>
+				</Grid>
+
+				{/* Recent Orders */}
+				<Grid item xs={12}>
+					<Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+						<TextField multiline fullWidth rows={4}></TextField>
+					</Paper>
+				</Grid>
 			</Grid>
-		</Grid>
+		</form>
 	);
 };
 export default AddProduct;
-/* 	<Controller
-										name="firstName"
-										control={control}
-										rules={{ required: true }}
-										render={({ field }) => <TextField {...field} />}
-									/>
-									
-
-						<Box>
-							<TextField label="Price" type="number"></TextField>
-						</Box>
-						<Box>
-							<Select
-								labelId="demo-simple-select-label"
-								value={age}
-								onChange={handleChange}
-								id="sale"
-								label="Sale"
-							>
-								<MenuItem value={10}>Ten</MenuItem>
-								<MenuItem value={20}>Twenty</MenuItem>
-								<MenuItem value={30}>Thirty</MenuItem>
-							</Select>
-						</Box>
-						<Box>
-							<TextField label="Amount" type="number"></TextField>
-						</Box>
-						<Box>
-							<Select
-								labelId="demo-simple-select-label"
-								value={age}
-								onChange={handleChange}
-								id="sale"
-								label="Sale"
-							>
-								<MenuItem value={10}>Ten</MenuItem>
-								<MenuItem value={20}>Twenty</MenuItem>
-								<MenuItem value={30}>Thirty</MenuItem>
-							</Select>
-						</Box>
-									
-									*/
