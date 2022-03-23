f174183417a365f812ffb25cf170781fb4179ccb
Thanh Tungg <trantungg910@gmail.com>
2022-03-23 16:35:36 +0700

    Feat: required form

--- frontend/src/features/SignIn.tsx
+++ frontend/src/features/SignIn.tsx
@@ -2,7 +2,7 @@ import { Button, FormControl } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface IFormInput {
-	email: String;
+	email: any;
 	password: String;
 }
 
@@ -32,12 +32,19 @@ export default function SignIn() {
 					type="email"
 					className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 					placeholder=" "
-					{...register("email", { required: true, minLength: 5 })}
+					{...register("email", {
+						required: true,
+						minLength: 15,
+						pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+					})}
 				/>
 				<label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
 					Email address
 				</label>
 				{errors.email && <span className="text-red-600">This field is required email</span>}
+				{errors?.email?.type == "minLength" && (
+					<span className="text-red-600"> Email more than 20 characters</span>
+				)}
 			</div>
 			<div className="relative z-0 mb-6 w-full group">
 				<input
--- frontend/src/features/SignUp.tsx
+++ frontend/src/features/SignUp.tsx
@@ -2,8 +2,8 @@ import { Button, FormControl } from "@mui/material";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface IFormInput {
-	email: String;
-	password: String;
+	email: any;
+	password: any;
 	username: String;
 }
 
@@ -33,12 +33,15 @@ export default function SignUp() {
 					type="text"
 					className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 					placeholder=" "
-					{...register("username", { required: true, minLength: 5 })}
+					{...register("username", {
+						required: true,
+						minLength: 5,
+					})}
 				/>
 				<label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
 					Username
 				</label>
-				{errors.username && <span className="text-red-600">This field is required username!</span>}
+				{errors.username && <span className="text-red-600">This field is required username</span>}
 			</div>
 
 			<div className="relative z-0 mb-6 w-full group ">
@@ -46,12 +49,19 @@ export default function SignUp() {
 					type="email"
 					className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 					placeholder=" "
-					{...register("email", { required: true, minLength: 5 })}
+					{...register("email", {
+						required: true,
+						minLength: 5,
+						pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+					})}
 				/>
 				<label className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
 					Email address
 				</label>
 				{errors.email && <span className="text-red-600">This field is required email</span>}
+				{errors?.email?.type == "minLength" && (
+					<span className="text-red-600"> Email more than 20 characters</span>
+				)}
 			</div>
 
 			<div className="relative z-0 mb-6 w-full group">
@@ -65,6 +75,9 @@ export default function SignUp() {
 					Password
 				</label>
 				{errors.password && <span className="text-red-600">This field is required password</span>}
+				{errors?.password?.type == "minLength" && (
+					<span className="text-red-600"> Password more than 8 characters</span>
+				)}
 			</div>
 
 			<Button variant="contained" onClick={handleSubmit(onSubmit)}>
