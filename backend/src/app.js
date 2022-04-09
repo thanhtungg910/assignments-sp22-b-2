d35459a8f78d9262fe7e1acc54b4e7f2199a9adf
Thanh Tungg <trantungg910@gmail.com>
2022-04-09 18:03:39 +0700

    Feat: chatbox

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
+const app = express();
 import cors from "cors";
 import morgan from "morgan";
+import http from "http";
 import dotenv from "dotenv";
-// import http from "http";
-import { Server } from "socket.io";
-/* import { readdirSync } from "fs";
-console.log("🚀 ~ file: app.js ~ line 6 ~ readdirSync", readdirSync("./src/routes")); */
+import socketio from "socket.io"
+
 import db from "./config/connect";
 import products from "./routes/products";
 import categories from "./routes/categories";
@@ -16,36 +16,53 @@ import messages from "./routes/messages";
 import conversation from "./routes/conversation";
 dotenv.config();
 import refreshToken from "./routes/refreshToken";
-import { addUser, getUser } from "./controllers/usersChat";
-const app = express();
-// const server = http.createServer(app);
-const io = new Server(3001, {
+// import { addUser, getUser } from "./controllers/usersChat";
+import { addUser, removeUser, getUser, getUsersInRoom } from "./userChat"
+const server = http.createServer(app);
+const io = socketio(server, {
 	cors: {
-		origin: [process.env.IO_URL] //Nếu không chạy đc 3001 thì chuyển tiếp tới đây
+		origin: process.env.IO_URL
 	}
 })
 
 // MIDDLEWARE
 app.use(morgan());
 app.use(express.json());
-// app.use(express.cookieParser())
 app.use(cors());
 //
 io.on("connection", (socket) => {
-	console.log("--->Socket connected<---");
-	socket.on('join', async (sender) => {
-		await addUser(sender, socket.id)
-		socket.broadcast.emit('message', { name: "admin", text: `Hello` });
-	})
-	socket.on("send-message", async ({ sender, receiver, msg }, callback) => {
-		const user = await getUser(receiver)
-		io.to(user.usersChat[1]).emit("message", { name: sender, text: msg })
+	console.log("🚀--->Socket connected<---");
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
+
+		if (error) return callback(error);
+
+		socket.join(user.room);
+		socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+		socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+		io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
 		callback();
-	})
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
 
 	socket.on("disconnect", () => {
-		console.log('disconnect');
+		console.log('🚀 disconnect');
+		const user = removeUser(socket.id);
+		if (user) {
+			io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+			io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+		}
 	})
+
 })
 //ROUTING
 app.use("/refreshtoken", refreshToken);
@@ -67,6 +84,6 @@ db.connect();
 
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log("Running port " + PORT);
 });
--- frontend/src/features/ChatsBox.tsx
+++ frontend/src/features/ChatsBox.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { io, Socket } from "socket.io-client";
 import { Fab, Snackbar, Button, Card, TextField } from "@mui/material";
+import { io } from "socket.io-client";
 import ChatIcon from "@mui/icons-material/Chat";
 import Chat from "../components/chats/Chat";
 import SendMessage from "../components/chats/SendMessage";
-import { DefaultEventsMap } from "@socket.io/component-emitter";
 import { getLocal, saveLocal } from "../utils/localstorage";
 import { createConversation } from "../api/chats";
 
-const ENDPOINT = "http://localhost:3001";
+const ENDPOINT = "http://localhost:5001";
 
-let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
 const ChatsBox = () => {
 	const [open, setOpen] = useState<boolean>(false);
 	const [sender, setSender] = useState<String>("");
@@ -18,17 +16,19 @@ const ChatsBox = () => {
 		[]
 	);
 	const [meg, setMsg] = useState<String>("");
-
+	const [socket, setSocket] = useState<any>(null);
+	const [room, setRomm] = useState("");
 	useEffect(() => {
-		socket = io(ENDPOINT, { transports: ["websocket"] });
+		setSocket(io(ENDPOINT, { transports: ["websocket"] }));
 	}, []);
 	useEffect(() => {
-		socket.on("message", (data: { name: String; text: String }) => {
-			console.log("data", data);
-
-			// setMessages(() => [...messages, { ...data }]);
+		socket?.on("message", (message: any) => {
+			console.log("🚀 => useEffect => message", message);
 		});
-	}, []);
+		socket?.on("roomData", (room: any) => {
+			console.log("🚀 => socket.on => room", room);
+		});
+	}, [socket]);
 
 	const handleOpen: () => void = () => {
 		setOpen(!open);
@@ -37,23 +37,14 @@ const ChatsBox = () => {
 		e: React.FormEvent
 	) => {
 		e.preventDefault();
-		socket.emit(
-			"send-message",
-			{
-				sender: getLocal("userChat"),
-				receiver: getLocal("userChat"),
-				msg: meg,
-			},
-			() => setMsg("")
-		);
+		socket?.emit("sendMessage", meg, () => setMsg(""));
 	};
 	const handleSaveLoginChat: (e: React.FormEvent) => void = async (
 		e: React.FormEvent
 	) => {
 		e.preventDefault();
 		//SEND
-		socket.emit("join", sender);
-
+		socket?.emit("join", { name: sender, room: room }, () => {});
 		saveLocal("userChat", sender);
 		setOpen(!open);
 	};
@@ -111,7 +102,11 @@ const ChatsBox = () => {
 								value={sender}
 								onChange={(e) => setSender(e.target.value)}
 							/>
-							<TextField placeholder="Phone" />
+							<TextField
+								placeholder="Phone"
+								value={room}
+								onChange={(e) => setRomm(e.target.value)}
+							/>
 							<Button type="submit">Send</Button>
 						</form>
 					)}
