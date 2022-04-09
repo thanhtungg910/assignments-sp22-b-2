d6120a05c99a6d0d19df20243e5909ed5d91a2b3
Thanh Tungg <trantungg910@gmail.com>
2022-04-09 10:00:53 +0700

    .

--- frontend/src/app/rootReducer.tsx
+++ frontend/src/app/rootReducer.tsx
@@ -2,12 +2,12 @@ import { combineReducers } from "redux";
 import cartReducer from "../slices/cart";
 import orderReducer from "../slices/order";
 import useReducer from "../slices/user";
-import wishListReducer from "../slices/wishlist";
+// import wishListReducer from "../slices/wishlist";
+// wishList: wishListReducer,
 
 const rootReducer = combineReducers({
 	users: useReducer,
 	carts: cartReducer,
-	wishList: wishListReducer,
 	orders: orderReducer,
 });
 export default rootReducer;
